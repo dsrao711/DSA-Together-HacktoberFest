414e1256337a663a26958daa1c0edf66afd8aa7a
Divya <dsrao0712@gmail.com>
2021-04-23 00:45:47 +0530

    Basic String Problem

---
+++ strings/checkPalindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPlaindrome(self, S):
+        # code here
+        reverse = S[::-1]
+        if (S == reverse):
+            op = 1
+        else:
+            op = 0
+        return op
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        S = input()
+        ob = Solution()
+        answer = ob.isPlaindrome(S)
+        print(answer)
+
+# } Driver Code Ends
