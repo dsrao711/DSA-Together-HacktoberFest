293cb81bd8064eaf29ff0e2a9dca3745e55681f3
Divya <dsrao0712@gmail.com>
2021-04-26 23:19:21 +0530

    Split the binary string into substrings with equal number of 0s and 1s

---
+++ strings/SplitBinaryString.py
@@ -0,0 +1,30 @@
+def SplitBinaryString(s , n):
+    
+    count_0 = 0
+    count_1 = 0 
+    
+    counter = 0
+    
+    for i in range(n):
+        if(s[i] == '0'):
+            count_0 += 1
+        else:
+            count_1 += 1
+            
+        if (count_0 == count_1):
+            counter += 1
+    
+    if count_0 != count_1 :
+        return -1
+            
+    return counter
+
+#Driver Code
+
+a ="010100110011"
+
+size = len(a)
+
+op = SplitBinaryString(a, size)
+
+print(op)
