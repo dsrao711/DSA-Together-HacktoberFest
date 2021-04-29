13fcff5e62dc3792ed5465c3ecb9b8068aaefb77
Divya <dsrao0712@gmail.com>
2021-04-30 01:53:46 +0530

    Count Squares

---
+++ searching_and_sorting/CountSquareGFG.py
@@ -0,0 +1,21 @@
+#https://practice.geeksforgeeks.org/problems/count-squares3649/1#
+
+class Solution:
+    def countSquares(self, N):
+        # code here 
+        return int((N - 1)**0.5)
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import math
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.countSquares(N))
+# } Driver Code Ends
