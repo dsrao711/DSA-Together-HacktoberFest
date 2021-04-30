3994dae24d4e900aa3f367fc1d0156c7d0132002
Divya <dsrao0712@gmail.com>
2021-05-01 02:49:37 +0530

    Find pair with given diff - GFG

---
+++ searching_and_sorting/FindPairGivenDifference.py
@@ -0,0 +1,47 @@
+
+class Solution:
+
+    def findPair(self, arr, L,N):
+        arr.sort()
+        #code here
+        i,j = 0,1
+        flag = 0
+        size = len(arr)
+        while( i < size and j < size):
+            
+            if(i != j and arr[j]-arr[i] == N):
+                flag = 1
+                break
+            
+            elif(arr[j] - arr[i] < N):
+                j +=1
+            else:
+                i +=1
+                
+        if(flag == 1):
+            return True
+        else:
+            return False
+            
+            
+                
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        L,N = [int(x) for x in input().split()]
+        arr = [int(x) for x in input().split()]
+
+        solObj = Solution()
+
+        if(solObj.findPair(arr,L, N)):
+            print(1)
+        else:
+            print(-1)
+# } Driver Code Ends
