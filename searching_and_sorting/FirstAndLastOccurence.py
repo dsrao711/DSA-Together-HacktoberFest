f235e7417e2b41878f30be32ac3b7265434012e6
Divya <dsrao0712@gmail.com>
2021-04-30 20:55:29 +0530

    First and Last Occurence

---
+++ searching_and_sorting/FirstAndLastOccurence.py
@@ -0,0 +1,35 @@
+# https://practice.geeksforgeeks.org/viewSol.php?subId=4c93efc2627e9e47ceb06b5f1aaa5d03&pid=703459&user=divyarao1
+
+def find(arr,n,x):
+    # code here
+    arr.sort()
+    op = []
+    for i in range(0 , n) :
+        if arr[i] == x :
+            op.append(i)
+            break
+    rev = arr[::-1]
+    for i in range (0 , n):
+        if (rev[i] == x):
+            op.append(n - i - 1)
+            break
+        
+    if(len(op) == 0):
+        return [-1 , -1]
+        
+    return op
+    
+    
+                
+
+#{ 
+#  Driver Code Starts
+t=int(input())
+for _ in range(0,t):
+    l=list(map(int,input().split()))
+    n=l[0]
+    x=l[1]
+    arr=list(map(int,input().split()))
+    ans=find(arr,n,x)
+    print(*ans)
+# } Driver Code Ends
