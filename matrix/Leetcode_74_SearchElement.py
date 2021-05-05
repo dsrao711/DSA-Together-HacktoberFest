ebfb4620e900421effd080c9b9d47c3008f80297
Divya <dsrao0712@gmail.com>
2021-05-05 23:49:06 +0530

    Explanation added

--- matrix/Leetcode_74_SearchElement.py
+++ matrix/Leetcode_74_SearchElement.py
@@ -1,36 +1,36 @@
-# https://leetcode.com/problems/search-a-2d-matrix/
+# Question : https://leetcode.com/problems/search-a-2d-matrix/
+
+# Search an element in array
+#Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than the last integer of the previous row.
+
+# Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+# Output: true
+
+#1. Apply Binary Search on every row 
+# Iterate through every row and consider each row as an array and apply binary Search
 
 class Solution:
-
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        output = False
-        rows = len(matrix)
-        columns = len(matrix[0])
-
-        if(rows == 1 and columns == 1):
-            # For 1x1 matrix like : matrix = [[1]] 
-            if(target == matrix[0][0]):
-                return True
-            else:
-                return False
-
-        result = False
-        j = len(matrix[0]) - 1
+    def searchMatrix(self, matrix, target):
         
-        for i in range(0 , rows):
-
-            if(target == matrix[i][j]):
-                result =  True
-            elif (target <= matrix[i][j]):
-                result = self.search(matrix[i] , target)
+        output = False
+        if len(matrix) == 1 and len(matrix[0]) == 1:
+            if target == matrix[0][0]:return True 
+            else:return False
+            
+        j = len(matrix[0]) - 1
+        for i in range(len(matrix)):
+            if target == matrix[i][j]:
+                return True
+            elif target < matrix[i][j]:
+                output = self.search(matrix[i], target)
                 break
-
-        return result
-
-# Implementing Binary Search 
-
-     def search(self, arr, target):
-            beg_index = 0
+        return output
+    
+    def search(self, arr, target):
+        beg_index = 0
         end_index = len(arr) - 1
         while beg_index <= end_index :
             mid = (beg_index + end_index ) // 2
@@ -41,6 +41,3 @@ class Solution:
             else:
                 beg_index = mid + 1
         return False
-
-
-       
