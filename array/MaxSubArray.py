39cabcd09f068cfaf39f55de2274633a79c1b4db
Divya <dsrao0712@gmail.com>
2021-05-12 22:53:04 +0530

    Leetcode - max sub array

---
+++ array/MaxSubArray.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/maximum-subarray/
+
+# Using Kadanes Algorithm
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxCurrent = maxGlobal = nums[0]
+        if (len(nums) == 1):
+          maxGlobal = nums[0]
+        elif(len(nums) == 0):
+          maxGlobal = -1
+        else:
+          for i in range(1 , len(nums)):
+            maxCurrent = max( nums[i]+maxCurrent , nums[i])
+
+            if(maxCurrent > maxGlobal):
+              maxGlobal = maxCurrent
+            
+        return maxGlobal
