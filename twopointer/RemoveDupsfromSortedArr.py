9f72740f3b4e1bdda7b1e7a6bd601d708d7a0163
Divya <dsrao0712@gmail.com>
2021-05-21 00:04:44 +0530

    Waste commit

--- twopointer/RemoveDupsfromSortedArr.py
+++ twopointer/RemoveDupsfromSortedArr.py
@@ -1,8 +1,11 @@
+# Remove Duplicates from Sorted Array
+
 # Link to problem : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
+# Its clearly stated in the question that a new space cannot be allocated
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums):
       i = 0
       while i < len(nums) - 1 :
         if nums[i] == nums[i+1]:
@@ -10,3 +13,5 @@ class Solution:
         else:
           i += 1
       return len(nums)
+  
+  
