00408cbcf944f7b2668a309adf6d6a189d797fbc
Divya <dsrao0712@gmail.com>
2021-05-27 03:35:21 +0530

    Hackerank Camel Case String easy problem

---
+++ strings/CamelCase.py
@@ -0,0 +1,35 @@
+# Hackerank : https://www.hackerrank.com/challenges/camelcase/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    # Write your code here
+    counter = 1
+    for i in s :
+      if(i.isupper()):
+        counter += 1
+    return counter
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
