9aae7e5db1b0ff4a149601a214b656867f3f20a5
Divya <dsrao0712@gmail.com>
2021-05-27 02:59:26 +0530

    Hackerank Height of Binary Tree

---
+++ trees/HeightOfBinTree.py
@@ -0,0 +1,56 @@
+# Link to the problem : https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+'''
+class Node:
+      def __init__(self,info): 
+          self.info = info  
+          self.left = None  
+          self.right = None 
+           
+
+       // this is a node of the tree , which contains info as data, left , right
+'''
+def height(root):
+  add = 0
+  if(root.left):
+    add = 1 + height(root.left)
+  if(root.right):
+    add = 1 + height(root.right)
+  return add
