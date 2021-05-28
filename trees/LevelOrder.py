3bf428006aee4864406f7852497fbfd8df521a38
Divya <dsrao0712@gmail.com>
2021-05-28 21:03:20 +0530

    Level order traversal - Hackerank

---
+++ trees/LevelOrder.py
@@ -0,0 +1,68 @@
+# Hackerrank : https://www.hackerrank.com/challenges/tree-level-order-traversal/problem
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def levelOrder(root):
+    #Write your code here
+    
+    queue = []
+    queue.append(root)
+    while (len(queue) > 0):
+      node = queue.pop(0)
+      print(str(node.info) + " " ,end = "")
+      if(node.left):
+        queue.append(node.left)
+      if(node.right):
+        queue.append(node.right)
+      
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+levelOrder(tree.root)
