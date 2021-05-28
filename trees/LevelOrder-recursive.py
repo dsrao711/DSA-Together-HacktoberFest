f321d6fef3d5de2493e9f9388d737bc7c5e9874d
Divya <dsrao0712@gmail.com>
2021-05-28 22:03:04 +0530

    Recursive approach for level order approach

---
+++ trees/LevelOrder-recursive.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# Recursive approach 
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+      result = []
+      self.TreeTraversal(root,0,result)
+      return result
+        
+    def TreeTraversal(self,node,level,result):
+      if(node == None):
+        return 
+      
+      if(len(result) < level + 1) :
+        result.append([])
+        
+      result[level].append(node.val)
+      self.TreeTraversal(node.left , level+1 , result)
+      self.TreeTraversal(node.right , level+ 1 , result)
