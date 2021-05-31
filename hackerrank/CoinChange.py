0922ee11832a25c601e733042c61bbd06cca325f
Divya <dsrao0712@gmail.com>
2021-06-01 02:49:36 +0530

    Coin Change

---
+++ hackerrank/CoinChange.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getWays' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. LONG_INTEGER_ARRAY c
+# Hackerank : https://www.hackerrank.com/challenges/coin-change/problem
+
+def getWays(amount, coins):
+    # Write your code here
+    dp = [1] + [0]*amount
+    for coin in coins:
+        for i in range(coin, amount+1):
+            dp[i] += dp[i-coin]
+    return dp[amount]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    # Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    ways = getWays(n, c)
+
+    fptr.write(str(ways) + '\n')
+
+    fptr.close()
