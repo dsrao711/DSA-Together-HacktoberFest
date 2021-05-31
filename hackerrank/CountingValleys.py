fce1234cff18ae636962076c197ae56f7c8fefa7
Divya <dsrao0712@gmail.com>
2021-05-31 18:21:07 +0530

    Counting valleys

---
+++ hackerrank/CountingValleys.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    sealevel = valley = 0
+    for i in path :
+      if i == 'U':
+        sealevel += 1
+      else:
+        sealevel -= 1
+      if(i == 'U' and sealevel == 0):
+        valley += 1
+        
+    return valley
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
