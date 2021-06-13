c8763041d9268f27c91875fb950be14cbd145ed9
Divya <dsrao0712@gmail.com>
2021-06-14 02:52:21 +0530

    Insertion in BST

--- trees/BST/InsertANodeinBST.py
+++ trees/BST/InsertANodeinBST.py
@@ -38,7 +38,6 @@ class BinarySearchTree:
           
       else: 
           cur.right = self.insertion(cur.right, val)
-
       return cur
         
         
@@ -67,3 +66,49 @@ for i in range(t):
     tree.insert(arr[i])
 
 preOrder(tree.root)
+
+
+#Leetcode : 
+#https://leetcode.com/problems/insert-into-a-binary-search-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        def insert(root,x):
+          if(root):
+            if(x < root.val):
+              # Insert Left
+              # If Left node is already present
+              if(root.left):
+                insert(root.left,x)        
+              #Else, create a new left node
+              else:
+                root.left = TreeNode(x)
+            else:
+              # Insert Right
+              # If Right node is already present
+              if(root.right):
+                insert(root.right , x)
+              # Else , create a new right node
+              else:
+                root.right = TreeNode(x)
+                
+          else:
+            return root
+            
+        if not root:
+          return TreeNode(val)
+        
+        insert(root , val)
+        
+        return root
+                
+              
+          
