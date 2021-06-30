9928a99bcd7e941de0025eb8f99df395f487daa9
Divya <dsrao0712@gmail.com>
2021-06-30 20:46:30 +0530

    Divisor game

---
+++ DynamicProgramming/DivisorGame.py
@@ -0,0 +1,12 @@
+#https://leetcode.com/problems/divisor-game/
+
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+  
+      if(n % 2 == 0):
+        return True
+      else:
+        return False
+        
+
+
