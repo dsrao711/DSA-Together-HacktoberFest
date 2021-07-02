d676c61c9698430fb9be483bdcc0d4944efa97e1
Divya <dsrao0712@gmail.com>
2021-07-02 21:08:54 +0530

    Count stairs

---
+++ DynamicProgramming/Count ways to reach the n’th stair.py
@@ -0,0 +1,22 @@
+#https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter5639/1/?category[]=Dynamic%20Programming&category[]=Dynamic%20Programming&company[]=Accolite&company[]=Morgan%20Stanley&company[]=BrowserStack&company[]=Accolite&company[]=Morgan%20Stanley&company[]=BrowserStack&page=1&query=category[]Dynamic%20Programmingcompany[]Accolitecompany[]Morgan%20Stanleycompany[]BrowserStackpage1company[]Accolitecompany[]Morgan%20Stanleycompany[]BrowserStackcategory[]Dynamic%20Programming#
+
+# Math approach
+
+class Solution:
+    def nthStair(self,n):
+        # Code here
+        return 1 + n//2
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        n = int(input())
+        ob = Solution();
+        ans = ob.nthStair(n)
+        print(ans)
+# } Driver Code Ends
