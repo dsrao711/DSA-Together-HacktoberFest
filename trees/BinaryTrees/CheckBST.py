cae58e6e260805a8afbe8a741a2bd34599b00848
Divya <dsrao0712@gmail.com>
2021-07-08 21:02:42 +0530

    Check if BST

--- trees/BinaryTrees/CheckBST.py
+++ trees/BinaryTrees/CheckBST.py
@@ -1,48 +1,31 @@
-# Python program to check if a binary tree is bst or not
-
-INT_MAX = 4294967296
-INT_MIN = -4294967296
-
-# A binary tree node
-class Node:
-
-	# Constructor to create a new node
-	def __init__(self, data):
-		self.data = data
-		self.left = None
-		self.right = None
-
-
-# Returns true if the given tree is a binary search tree
-# (efficient version)
-def isBST(node):
-	return (isBSTUtil(node, INT_MIN, INT_MAX))
-
-# Retusn true if the given tree is a BST and its values
-# >= min and <= max
-def isBSTUtil(node, mini, maxi):
-	
-	# An empty tree is BST
-	if node is None:
-		return True
-
-	# False if this node violates min/max constraint
-	if node.data < mini or node.data > maxi:
-		return False
-
-	# Otherwise check the subtrees recursively
-	# tightening the min or max constraint
-	return (isBSTUtil(node.left, mini, node.data -1) and
-		isBSTUtil(node.right, node.data+1, maxi))
-
-# Driver program to test above function
-root = Node(4)
-root.left = Node(2)
-root.right = Node(5)
-root.left.left = Node(1)
-root.left.right = Node(3)
-
-if (isBST(root)):
-	print ("Is BST")
-else:
-	print ("Not a BST")
+# https://leetcode.com/problems/subtree-of-another-tree/submissions/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+  
+    def sameTree(self , root , subRoot):
+      if(root == None or subRoot == None):
+        return root == None and subRoot == None
+      elif(root.val == subRoot.val):
+        return self.sameTree(root.right , subRoot.right) and self.sameTree(root.left , subRoot.left)
+      else:
+        return False
+      
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """
+        if(root == None):
+          return False
+        elif(self.sameTree(root , subRoot)):
+          return True
+        else:
+          return self.isSubtree(root.right , subRoot) or self.isSubtree(root.left , subRoot)
+        
+        
