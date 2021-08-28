73eafc3168ad23836cbf59e942bdc71ed1afbeb3
Divya <dsrao0712@gmail.com>
2021-08-28 17:09:31 +0530

    Minimum deletion cost - approach 1

--- Greedy/medium/MinimumDeletionCost.py
+++ Greedy/medium/MinimumDeletionCost.py
@@ -1,5 +1,17 @@
 # https://leetcode.com/problems/minimum-deletion-cost-to-avoid-repeating-letters/submissions/
 
+# Approach1 - 
+# We need to return the minimum cost required to delete the duplicates in a list
+# We need to delete those duplicates with minimum cost 
+
+# Variables :
+#prev_index = 0
+# i = 1
+# total_cost = 0
+# We iterate through the string  to check duplicates , if a duplicate is found i.e
+# If s[i] == s[prev_index] --> total_cost += min(s[i] , s[prev_index]) , shift the prev_index to i
+# Else : Update the prev_index to i 
+# Return total_cost
 class Solution(object):
     def minCost(self, s, cost):
         """
@@ -14,10 +26,8 @@ class Solution(object):
         while(i < n):
           if(s[prev_index] == s[i]):
             total_cost += min(cost[prev_index] , cost[i])
-            
             if(cost[prev_index] < cost[i]):
               prev_index = i
-              
           else:
             prev_index = i
           
