4763f1a19eb16cc47e641bfa62a0e7a6c3eb6c2d
Divya <dsrao0712@gmail.com>
2021-08-28 16:13:58 +0530

    Deletion cost

---
+++ Greedy/medium/MinimumDeletionCost.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/minimum-deletion-cost-to-avoid-repeating-letters/submissions/
+
+class Solution(object):
+    def minCost(self, s, cost):
+        """
+        :type s: str
+        :type cost: List[int]
+        :rtype: int
+        """
+        total_cost = 0
+        prev_index = 0
+        i = 1
+        n = len(s)
+        while(i < n):
+          if(s[prev_index] == s[i]):
+            total_cost += min(cost[prev_index] , cost[i])
+            
+            if(cost[prev_index] < cost[i]):
+              prev_index = i
+              
+          else:
+            prev_index = i
+          
+          i += 1
+          
+        return total_cost
