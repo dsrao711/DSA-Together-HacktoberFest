1bfce0c66490ec6ca3b79825868b5f6c309c0f68
Divya <dsrao0712@gmail.com>
2021-09-04 01:53:38 +0530

    armstrong

---
+++ array/Easy/Armstrong.py
@@ -0,0 +1,25 @@
+class Solution:
+    def armstrongNumber (ob, n):
+        # code here 
+        number = str(n)
+        arm = 0
+        for i in range(0 , len(number)):
+            arm += pow(int(number[i]) , 3)
+        if(arm == n):
+            return "Yes"
+        else:
+            return "No"
+            
+            
+            
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n = input()
+        n=int(n)
+        ob = Solution()
+        print(ob.armstrongNumber(n))
+# } Driver Code Ends
