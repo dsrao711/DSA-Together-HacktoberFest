9e2934d9289c2ec5c2a3fade590d8559161d2a79
Divya <dsrao0712@gmail.com>
2021-09-12 02:30:03 +0530

    stack approach - backspace string

---
+++ stacks/Easy/BackspaceStringCompare.py
@@ -0,0 +1,40 @@
+# Link : https://leetcode.com/problems/backspace-string-compare/
+
+# Stack approach
+
+# Iterate through the string and push elements in a stack after checking if the element is a "#"
+# If the element is a "#" , pop the top ele of the stack
+# If not , simply push the ele into the stack 
+# Convert the stack into a string
+
+# Do this process for both s and t 
+# Return true if the o/p strings of s and t are equal
+
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        def build(S):
+        
+          stack = []
+
+          for i in S : 
+            if i != "#":
+              stack.append(i)
+            elif stack : 
+              stack.pop()
+              
+          return "".join(stack)
+        
+        return build(s) == build(t)
+            
+        
+# Complexity Analysis
+
+# Time Complexity: O(M + N)O(M+N), where M, NM,N are the lengths of S and T respectively.
+
+# Space Complexity: O(M + N)
+
