fbc35b8f6eb5492c60e073e7d01cd6420a5d9c89
Divya <dsrao0712@gmail.com>
2021-09-29 17:29:20 +0530

    Trees - Range sum BST - Recursive approach - easy

--- trees/BinaryTrees/Easy/RangeSumofBST.py
+++ trees/BinaryTrees/Easy/RangeSumofBST.py
@@ -1,5 +1,7 @@
 # Link : https://leetcode.com/problems/range-sum-of-bst/submissions/
 
+# YT Video ref : https://www.youtube.com/watch?v=iQiEMgbptwQ
+
 #Approach 1 : Stacks 
 
 # Definition for a binary tree node.
@@ -8,6 +10,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution(object):
     def rangeSumBST(self, root, low, high):
         """
@@ -38,5 +41,48 @@ class Solution(object):
               st.append(curr.right)
               
         return range_sum
+              
+        
+        
+# Approach 2 : Recursion 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rangeSumBST(self, root, low, high):
+        """
+        :type root: TreeNode
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        
+        self.total = 0
+        
+        def rangeSum (root ):
+          
+          if not root :
+            return None
+          
+          if(root.val >= low and root.val <= high):
+            self.total += root.val
+          
+          if(root.val >= low):
+            rangeSum(root.left)
+            
+          if(root.val <= high):
+            rangeSum(root.right)
+          
+          
+        rangeSum(root)
+        
+        return self.total
+          
+        
+        
               
         
