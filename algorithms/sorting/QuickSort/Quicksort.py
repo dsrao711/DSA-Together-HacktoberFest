517329ad7354cbc1084dd268039479598facf188
Siddhesh4501 <pawarsd20.comp@coep.ac.in>
2021-10-03 22:22:53 +0530

    Taking first index as pivot element

--- algorithms/sorting/QuickSort/Quicksort.py
+++ algorithms/sorting/QuickSort/Quicksort.py
@@ -1,17 +1,6 @@
-def quicksort(sequence) :
-    length = len(sequence)
-    if (length <= 1):
-        return sequence
-    else : 
-        pivot = sequence.pop()
-
-    lower_sequence = []
-    upper_sequence = []
-    for item in sequence :
-        if(item < pivot):
-            lower_sequence.append(item)
-        else: 
-            upper_sequence.append(item)
-    return quicksort(lower_sequence) + [pivot] + quicksort(upper_sequence) 
-
-print(quicksort([1 , 5 , 6, 3, 5, 3, 0]))
+def QuickSort(l):
+    if(len(l)<=1):
+        return l
+    return (QuickSort([i for i in l[1:] if i<l[0]]) + ([i for i in l if i==l[0]]) + QuickSort([i for i in l[1:] if i>l[0]]))
+l=[1,4,3,5,7,4,5,3,7,1,5,8,3,6]
+print(QuickSort(l))
