a02fbce863c77fd8d06550b00cf50c8e37030339
Monu Yadav <69475573+iam-monu@users.noreply.github.com>
2021-10-03 15:38:48 +0530

    Added patterns code with Screenshot of same!
    Do add this to your project and help me win goodies!

---
+++ Patterns/pyramid.py
@@ -0,0 +1,15 @@
+def pattern(n):
+    k = 0
+
+    for i in range(1, rows+1):
+        for space in range(1, (rows-i)+1):
+            print(end="  ")
+    
+        while k!=(2*i-1):
+            print("* ", end="")
+            k += 1
+    
+        k = 0
+        print()
+rows = int(input("Enter number of rows: "))
+pattern(rows)
