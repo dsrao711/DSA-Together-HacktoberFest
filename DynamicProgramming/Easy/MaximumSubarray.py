561122448f5027275fbb478d94e050b3f3371d3a
[krishnak3101] <[krishnakothagorla@gmail.com]>
2021-10-05 23:30:39 +0530

    MaximumSubarray Optimize 2

--- DynamicProgramming/Easy/MaximumSubarray.py
+++ DynamicProgramming/Easy/MaximumSubarray.py
@@ -4,7 +4,27 @@
 
 # Refer Kadanes algo solution in Arrays
 
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Dp approach
 
+        dp = [0]*(len(nums))
+        dp[0] = nums[0]
+
+        for i in range(1 , len(nums)):
+          if(nums[i] < nums[i] + dp[i-1]):
+            dp[i] = nums[i] + dp[i-1]
+          else:
+            dp[i] = nums[i]
+
+        return max(dp)
+
+
+# With O(1) space , optimized solution
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
 
