58494796f44892602ffe05877974a795d455d4c1
[krishnak3101] <[krishnakothagorla@gmail.com]>
2021-10-05 22:50:57 +0530

    Optimize Maximum Subarray

--- DynamicProgramming/Easy/MaximumSubarray.py
+++ DynamicProgramming/Easy/MaximumSubarray.py
@@ -4,21 +4,27 @@
 
 # Refer Kadanes algo solution in Arrays
 
-class Solution(object):
-    def maxSubArray(self, nums):
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
         """
-        :type nums: List[int]
-        :rtype: int
+            The idea of this approach is to traverse the array linearly and update the array such that arr[i] holds the
+            maximum sum of all the sub arrays ending at index i.
+
+            While traversing when we are at index k (k>0), arr[k-1] contains the maximum sum of all the sub arrays ending at index k-1.
+            So if arr[k-1] is positive we will update arr[k] as arr[k] = arr[k] + arr[k-1].
+            otherwise , we will not do anything because the array [arr[k]] is the sub array which gives maximum sum of all
+            the sub arrays ending at index k.
+
+            Time complexity :- O(n) because we are linearly traversing the array once.
+            Space complexity :- O(1) because we are not using any extra space.
+
         """
-        # Dp approach 
-        
-        dp = [0]*(len(nums))
-        dp[0] = nums[0]
-        
-        for i in range(1 , len(nums)):
-          if(nums[i] < nums[i] + dp[i-1]):
-            dp[i] = nums[i] + dp[i-1]
-          else:
-            dp[i] = nums[i]
-            
-        return max(dp)
+
+        for i in range(1, len(nums)):
+            if nums[i-1] > 0:
+                nums[i] = nums[i] + nums[i-1]
+
+        return max(nums)
+
