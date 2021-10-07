735f1a04477f8ab7daa48b754b5a75ccdb89f8bf
Vedant Shrivastava <vedantshrivastava466@gmail.com>
2021-10-07 09:59:51 +0530

    Update reverseDoublyLinkedList.py

--- linkedlist/reverseDoublyLinkedList.py
+++ linkedlist/reverseDoublyLinkedList.py
@@ -1,15 +1,7 @@
 20. Reverse a Doubly Linked list.
 Here is a simple method for reversing a Doubly Linked List. 
 All we need to do is swap prev and next pointers for all nodes, change prev of the head (or start) and change the head pointer in the end.
-* Function to reverse a Doubly Linked List */ 
- 
- 
-Time Complexity: O(N), where N denotes the number of nodes in the doubly linked list. 
-Auxiliary Space: O(1) 
-    We can also swap data instead of pointers to reverse the Doubly Linked List. 
-    Method used for reversing array can be used to swap data. Swapping data can be costly compared to pointers if the size of the data item(s) is more. Method 2: The same question can also be done by using Stacks. Steps:
-1. Keep pushing the node’s data in the stack. -> O(n) 
-2. The keep popping the elements out and updating the Doubly Linked List *Function to reverse a Doubly Linked List using Stacks * Function to reverse a Doubly Linked List using Stacks */ 
+   
 class Node:
  
     # Constructor to create a new node
