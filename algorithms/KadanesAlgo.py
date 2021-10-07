63057376ae4a03005d003f7cdde0415900867095
Divya <dsrao0712@gmail.com>
2021-10-07 23:03:10 +0530

    updates fixed

--- algorithms/KadanesAlgo.py
+++ algorithms/KadanesAlgo.py
@@ -1,5 +1,10 @@
+# Link : https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+
+
+##Complete this function
 def maxSubArraySum(a,size):
-   
+    ##Your code here
+    
    max_current = a[0]
    max_global = max_current
    for i in range (1 , size):
@@ -7,3 +12,26 @@ def maxSubArraySum(a,size):
        max_global = max(max_current , max_global)
    return max_global
     
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+ 
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            print(maxSubArraySum(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
