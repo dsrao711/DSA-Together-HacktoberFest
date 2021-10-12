0323e446cc9c4109f0cbd2e88716e4c46e4192f2
Krishan Kant Agnihotri <60218702+KrishanKantAgnihotri@users.noreply.github.com>
2021-10-12 11:25:29 +0530

    Update Find-Missing-Observations.py

--- Math/Medium/Find-Missing-Observations.py
+++ Math/Medium/Find-Missing-Observations.py
@@ -3,20 +3,22 @@ class Solution:
     def missingRolls(self, rolls, mean, n):
         m = len(rolls)
         ans =[]
-        tot = mean*(n+m)
+        tot = mean*(n+m) # total sum of rolls
         sm = 0 
         for i in range(m):
-            sm+=rolls[i]
-        tot-=sm
+            sm+=rolls[i]   # sum of m observation
+        tot-=sm            # sum of remaining n observation
         if tot<n or tot>6*n:
-            return ans
+            return ans      # return empty if sum is not possible    
         
-        av = tot//n
+
+        av = tot//n         # calculating average to assign each entry with it
         for i in range(n):
             ans.append(max(av,1))
-            tot-=max(av,1)
+            tot-=max(av,1)  
         i = 0  
-        while tot>0:
+        # now we are trying to increase every entry greedly until we hit the total sum
+        while tot>0:    
             val =(min(6-ans[i],tot))
             tot-=val
             ans[i] +=val
