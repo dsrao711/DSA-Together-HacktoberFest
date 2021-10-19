1c912aec33c7a25e67a0d6d3db2d50eb97a920c6
Siddharth Bhatnagar <siddharthb1604@gmail.com>
2021-10-19 16:19:31 +0530

    Palindrome leetcode

---
+++ Math/Easy/Palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:# function to ceck if plaindrome
+        a=0
+        revx=x
+        if(x<0):#for negative numbers as -121 is not equal to 121- 
+            return False;
+        while x != 0:# loop to reverse number
+            digit = x % 10
+            a = a * 10 + digit
+            x //= 10
+        if(a == revx):# to check if its plaindrome
+            return True
+        else:
+            return False
