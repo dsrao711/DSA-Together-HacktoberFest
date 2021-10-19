f64584cb5d51b081dc229d901e369a882a5eb201
Siddharth Bhatnagar <siddharthb1604@gmail.com>
2021-10-19 18:07:59 +0530

    Palindrome leetcode updated

--- Math/Easy/Palindrome.py
+++ Math/Easy/Palindrome.py
@@ -1,9 +1,12 @@
+#Link to prob:https://leetcode.com/problems/palindrome-number/
+
+
 class Solution:
-    def isPalindrome(self, x: int) -> bool:# function to ceck if plaindrome
+    def isPalindrome(self, x: int) -> bool:# function to check if plaindrome
         a=0
         revx=x
-        if(x<0):#for negative numbers as -121 is not equal to 121- 
-            return False;
+        if(x<0):#for negative numbers as -121 is not equal to 121-
+            return False
         while x != 0:# loop to reverse number
             digit = x % 10
             a = a * 10 + digit
@@ -12,3 +15,6 @@ class Solution:
             return True
         else:
             return False
+
+
+#TIme complexity: O(logN)        
