bb80af157008e6be9bbb601ebaa9ebc920076da1
Princeton21 <princeton2102@gmail.com>
2021-10-22 01:13:43 +0530

    Added time complexity and comments in Saddle point.py

--- matrix/Saddle point.py
+++ matrix/Saddle point.py
@@ -5,22 +5,37 @@ col=int(input("Enter the number of column : "))
 l=[(list(map(int,input().split()))) for i in range(col)]
 flag=0
 
-for i in range(row):
-    max = 0
-    min = l[i][0]
-    index = 0
-    for j in range(col):
-        if (min > l[i][j]):
+for i in range(row):            #O(n)
+    max = 0                   #|
+    min = l[i][0]             #|-->O(1)
+    index = 0                 #|
+
+    #first we find the minimum element in the jth row
+    for j in range(col):        #O(n)
+        if (min > l[i][j]):     #O(c1)
             min = l[i][j]
+            #after we find the minimum element in the row,
+            #we store its column value in the index variable
             index = j
-    for k in range(row):
-        if (l[k][index] > max):
+
+    #now we find the maximum element in the above stored column
+    for k in range(row):        #O(n)
+        if (l[k][index] > max): #O(c2)
             max = l[k][index]
 
-    if max==min:
+    #To satisfy condition of saddle point,
+    #We compare that the max and min elements are the same
+    if max==min:                #O(c3)
         print("The saddle point is present at ", min)
         flag = 1
-if flag==0:
+if flag==0:                     #O(1)
     print("There is no saddle point present in the given matrix")
 
+'''
+Total Time Complexity :
+
+=O(n*(O(n)*c1 + O(n)*c2+c3+O(1))  ....(c1,c2,c3 are constants(same number of iterations)))
+=O(n*(2*O(n)))                    ....(neglecting negligible iterations)
+=O(n**2)
+'''
 
