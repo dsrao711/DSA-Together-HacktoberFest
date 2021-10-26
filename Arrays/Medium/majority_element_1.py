de820c67ddeb273dc5bbeec204940060c54229b0
deathsurgeon1 <schaudhary0104@gmail.com>
2021-10-26 19:50:47 +0530

    Added hashmap approach for majority element

--- Arrays/Medium/majority_element_1.py
+++ Arrays/Medium/majority_element_1.py
@@ -43,7 +43,28 @@ Return the element with the max count in the hashmap
 """
 # Code here
 
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        majority_element = -1
+        majority_element_count = 0
 
+        # Hashmap to store element count in the list
+        element_frequency_map = {}
+
+        for i in range(0, len(nums)):
+            current_element_count = element_frequency_map[nums[i]]
+            current_element_count += 1
+            # check if current element is majority element
+            if majority_element_count < current_element_count:
+                majority_element_count = current_element_count
+                majority_element = nums[i]
+            element_frequency_map[nums[i]] = current_element_count
+
+        return majority_element
 
 """
 Best approach
@@ -51,7 +72,7 @@ T.C = O(n)
 S.C = O(1)
 Moore's Voting Algorithm - Google interview question
 """
-# Code 
+# Code
 
 class Solution(object):
     def majorityElement(self, nums):
@@ -63,7 +84,7 @@ class Solution(object):
         ans_index = 0
         counter = 0
         n = len(nums)
-        
+
         for i in range(0 , n):
             # Check if the ans_index is equal to array element , if yes , increase the counter or else reduce it
             if(nums[i] == nums[ans_index]):
@@ -77,13 +98,12 @@ class Solution(object):
         # Confirm if the result of this algorithm is correct by finding the frequency of this element
         freq = 0
         res = -1
-        
+
         for i in range(0 , n):
             if(nums[i] == nums[ans_index]):
-                freq += 1 
-        # If the freq is greater than n/2 , it satisfies our condition      
+                freq += 1
+        # If the freq is greater than n/2 , it satisfies our condition
         if(freq >= (n/2)):
             res = nums[ans_index]
-            
+
         return res
-            
