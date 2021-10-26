c78ded0f23a3445c7d01f8fe71b62d5409acd9a4
Rajat Jha <rajatjha@Rajats-MacBook-Air.local>
2021-10-26 22:54:58 +0530

    Adding approach and Steps logic

--- DynamicProgramming/Medium/bricks_game.py
+++ DynamicProgramming/Medium/bricks_game.py
@@ -1,5 +1,21 @@
 #Problem Link :- https://www.hackerrank.com/challenges/play-game/problem
 #Code 
+
+"""
+The ideas behind this approach is this :
+> At any step, no matter the player, he will always make the optimal move
+> We do a bottom up approach, meaning we start from the end of the array and calculate the optimal move at that step (ex: if we have only 1 brick left what is the optimal move? what if we have 2 , what if 3, etc . . .)
+
+The variables used :
+> rev_arr: contains the reversed array; we reverse the array to start from the bottom and go all the way to the top
+> process: contains the optimal move calculated at every step
+> sum_arr: the cumulative sum of the elements in the array
+
+The steps :
+> for the first 3 steps since we have a maximum of 3 bricks the optimal move is to pick all the bricks we have so we calculate the value of the picked bricks and we store it in the process array
+> from the 3rd step : have 3 cases : pick one, two or three bricks. No matter the number of bricks we pick we are sure that the next move made by the second player will be the optimal move of the remaining and that value is stored in the process array. so we know the value of our pick and the value of the next move
+"""
+
 #!/bin/python
 
 import math
