2a77c7000951d0a78a0a8d93f635d32a7e32ee3f
dsrao711 <dsrao0712@gmail.com>
2021-12-30 01:17:40 +0530

    plus_one

---
+++ DynamicProgramming/Easy/plus_one.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/plus-one/
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        res = 0
+        for digi in digits:
+            res=res*10+digi
+        res+=1
+        res = list(str(res))
+        return res
