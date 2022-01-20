b573fecf4452c8354c25224204146e51a1f5d981
dsrao711 <dsrao0712@gmail.com>
2022-01-20 22:37:55 +0530

    am-wow-pascals

---
+++ amazon/pascals_triangle.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
+        m = [[1]*i for i in range(1 , numRows  + 1 )]
+        
+        for i in range(numRows):
+            for j in range(1 , len(m[i]) - 1):
+                m[i][j] = m[i-1][j-1] + m[i-1][j]
+        
+        return m
