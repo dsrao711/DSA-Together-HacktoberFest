7ffa7d15d6da278c60858998ed76740f19fa2193
dsrao711 <dsrao0712@gmail.com>
2022-01-26 13:46:05 +0530

    best_time_2

---
+++ amazon/stocks/best_time_2.py
@@ -0,0 +1,30 @@
+# question : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/
+# ref : https://youtu.be/HWJ9kIPpzXs
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        bd = 0 
+        sd = 0
+        profit = 0 
+        
+        for i in range (1 , len(prices)):
+            # Check for highs 
+            if(prices[i] > prices[i-1]):
+                sd += 1 
+            else:
+                # Dip found
+                # Calc the previous profit
+                profit += prices[sd] - prices[bd]
+                # Reinitialize new buying and selling day 
+                bd = i 
+                sd = i
+                
+        # If a stroke is found but the loop ends, calculate the remaining stroke
+        profit += prices[sd] - prices[bd]
+        
+        return profit
+        
