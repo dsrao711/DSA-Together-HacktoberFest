43c5cf16d1c3b4c62f44bda8438f0e819c05d8a1
dsrao711 <dsrao0712@gmail.com>
2022-02-15 21:43:32 +0530

    dfs

--- graphs/DFS.py
+++ graphs/DFS.py
@@ -1,53 +1,43 @@
-# Python3 Program to print DFS traversal
-# from a given source vertex. DFS(int s,boolean[] visited)
-# traverses vertices reachable from s.
 from collections import defaultdict
 
-# This class represents a directed graph
-# using adjacency list representation
-
-
-class Graph:
-
-    # Constructor
+class Graph :
     def __init__(self):
-
-        # default dictionary to store graph
         self.graph = defaultdict(list)
-
-    # function to add an edge to graph
-    def addEdge(self, u, v):
+        
+    def add_edge(self , u , v):
         self.graph[u].append(v)
+        
+    def dfsUtil(self , s , vis):
+        stack = []
+        stack.append(s)
+        while(stack):
+            # Pop
+            node = stack.pop()
+            # Mark visited
+            if(vis[node] == False):
+                vis[node] = True
+                print(node)
+            # Add elements from adj list in stack and mark visited
+            for i in self.graph[node]:
+                if(vis[i] == False):
+                    stack.append(i)
+                    vis[i] = True
+    
+                    
+    def dfs(self):
+        v = len(self.graph)
+        vis = [False]*(v+1)
+        
+        for i in range(v):
+            if(vis[i] == False):
+                self.dfsUtil(i , vis)
+                
 
-    # Function to print a DFS of graph
-    def DFS(self, s, visited):
-        # Mark the source node as visited
-        visited[s] = True
-        # Print the visited node
-        print(s, end=" ")
-
-        for i in self.graph[s]:
-            # already visited
-            if visited[i] == True:
-                continue
-            # recur to the next neighbor node
-            self.DFS(i, visited)
-
-
-# Driver code
-# Create a graph given in
-# the above diagram
-g = Graph()
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
-
-print("Following is Depth First Traversal"
-      " (starting from vertex 2)")
-# Mark all the vertices as not visited
-visited = [False] * (max(g.graph) + 1)
-# DFS call
-g.DFS(2, visited)
+if __name__ == '__main__' :
+    graph = Graph()
+    graph.add_edge(1, 0 )
+    graph.add_edge(1, 4)
+    graph.add_edge(2 , 1)
+    graph.add_edge(3 , 4)
+    graph.add_edge(4 , 0)
+    graph.dfs()
